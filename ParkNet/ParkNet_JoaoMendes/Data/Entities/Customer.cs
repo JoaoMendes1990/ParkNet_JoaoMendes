572cb4498efb9ee4655c5c18c04d09324afd4dc8
JoaoMendes1990 <jp.pinhao@gmail.com>
2024-02-07 22:36:11 +0000

    Criação de projeto e entidades

---
+++ ParkNet/ParkNet_JoaoMendes/Data/Entities/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkNet_JoaoMendes.Data.Entities
+{
+    public class Customer
+    {
+        // Properties
+        [Key]
+        public int CostumerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+       
+        [Required]
+        public string DriversLicense { get; set; }
+       
+        [Required]
+        public string CreditCard { get; set; }
+
+        public int VehicleId { get; set; }
+     //Falta criar o resto das entidades  
+    }
+}
